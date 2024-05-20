db9fa61815c3de25b1f8310b99e459b1a952509d
Ayub Hareed <69177111+A-Hareed@users.noreply.github.com>
2024-05-20 14:53:33 +0800

    Update set.sh

--- set.sh
+++ set.sh
@@ -2,6 +2,9 @@
 
 gmx pdb2gmx -f 2BEG_model1_capped.pdb -ignh -ter -o complex.gro
 
+gmx editconf -f complex.gro -o newbox.gro -center 3.280 2.181 2.4775 -box 6.560 4.362 12
+
+
 gmx solvate -cp newbox.gro -cs spc216.gro -o solv.gro -p topol.top
 
 gmx grompp -f ions.mdp -c solv.gro -p topol.top -o ions.tpr
