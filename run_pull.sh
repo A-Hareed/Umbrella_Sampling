2a40621c1929a1b7911d335c68136dc771acec2f
Ayub Hareed <69177111+A-Hareed@users.noreply.github.com>
2024-05-23 11:25:48 +0800

    Update run_pull.sh

--- run_pull.sh
+++ run_pull.sh
@@ -6,3 +6,6 @@ gmx make_ndx -f npt.gro
  > r 28-54
  > name 20 Chain_B
  > q
+
+gmx grompp -f md_pull.mdp -c npt.gro -p topol.top -r npt.gro -n index.ndx -t npt.cpt -o pull.tpr
+gmx mdrun -deffnm pull -pf pullf.xvg -px pullx.xvg
